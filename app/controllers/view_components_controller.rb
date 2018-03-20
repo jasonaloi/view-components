0ccde460f27394b3cd6d2f78fc384923f9605146
Jason Aloi <j@jasonaloi.com>
2018-03-20 03:48:43 -0500

    Sort view components and examples

--- app/controllers/view_components_controller.rb
+++ app/controllers/view_components_controller.rb
@@ -12,7 +12,7 @@ class ViewComponentsController < ApplicationController
   private
 
   def get_view_components
-    @view_components = Dir["app/views/application/*.html.*"]
+    @view_components = Dir["app/views/application/*.html.*"].sort
     return [] if @view_components.empty?
     pattern = /app\/views\/application\/(.*)\.html\.*/
     @view_components.map! {|f| f.scan(pattern).last.last}
@@ -21,7 +21,7 @@ class ViewComponentsController < ApplicationController
   def get_examples(view_component)
     @examples = Dir["app/views/application/examples/#{view_component}.html.*"]
     return false if @examples.empty?
-    @examples.concat Dir["app/views/application/examples/#{view_component}--*.html.*"]
+    @examples.concat Dir["app/views/application/examples/#{view_component}--*.html.*"].sort
     pattern = /app\/views\/application\/examples\/(.*)\.html\.*/
     @examples.map! {|f| f.scan(pattern).last.last}
   end
